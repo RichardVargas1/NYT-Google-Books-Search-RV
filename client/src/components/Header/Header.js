735d9fc8c6b389ea5458327533a25b93a3887728
RichardVargas1 <richard.vargas95@gmail.com>
2019-01-15 21:32:21 -0600

    Adding Header Files

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => (
+    <div className="header">
+        <h1 className="text-center">
+            <strong>
+                <i className></i> New York Times Search Application
+      </strong>
+        </h1>
+    </div>
+);
+
+export default Header;
